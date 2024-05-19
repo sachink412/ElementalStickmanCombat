8c908988c12e55d247cc06fd4a77a28c560db0db
Sachin Khanal <sachinkhanal412@gmail.com>
2024-05-18 23:58:01 -0400

    added image to title screen, need to fix the layout of the buttons

--- game/scene/TitleScreen.java
+++ game/scene/TitleScreen.java
@@ -1,17 +1,39 @@
 package game.scene;
 
+import game.Game;
+
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.util.*;
 import java.lang.StringBuffer;
+import java.awt.Graphics;
 
 public class TitleScreen extends JPanel {
-
     public HashMap<String, JButton> buttons = new HashMap<String, JButton>();
     public StringBuffer currentButton = new StringBuffer();
+    private Image background = Toolkit.getDefaultToolkit().getImage("game/assets/images/Title.png")
+            .getScaledInstance(Game.WINDOW_WIDTH, Game.WINDOW_HEIGHT, Image.SCALE_SMOOTH);;
 
     public TitleScreen() {
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+        this.setFocusable(true);
+        this.requestFocus();
+        this.setVisible(true);
+
+        JLabel title = new JLabel("ELEMENTAL STICKMAN COMBAT");
+        title.setFont(new Font("Arial", Font.BOLD, 50)); // Set the font size and style
+        title.setHorizontalAlignment(JLabel.CENTER); // Center align the text
+        title.setForeground(Color.WHITE); // Set the text color
+
+        this.add(title);
+
         JButton startButton = new JButton("Start");
         JButton singlePlayerButton = new JButton("Single Player");
         JButton multiPlayerButton = new JButton("Multi Player");
@@ -57,5 +79,11 @@ public class TitleScreen extends JPanel {
             exitButton.setVisible(false);
             System.out.println("Exit");
         });
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(background, 0, 0, this);
     }
 }
